2daf4227dd39ebd86a1c01bbe807ffa88a8010ba
lehung2022 <hung.le.programmer@gmail.com>
2023-06-16 16:05:25 +0700

    react chapter 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import '../index.css';
+
+const Header = ({ title }) => {
+
+  return (
+      <header>
+          <h4>{title}</h4>
+      </header>
+  )
+}
+
+Header.defaultProps = {
+  title: "MK Characters Lists"
+}
+
+export default Header;
